7f1978119930b58a04e1850f9ebad99b0d305829
JD1714 <jd19122003@gmail.com>
2026-01-09 14:20:36 -0400

    feat: add comment for CreateChatModal in ChatHub component

--- src/presentation/components/features/chat/chat-hub.tsx
+++ src/presentation/components/features/chat/chat-hub.tsx
@@ -46,6 +46,7 @@ export const ChatHub = () => {
         //Simulate background
         <div className="min-h-screen w-full bg-linear-to-br bg-black flex items-center justify-center p-4 md:p-8 font-sans">
 
+            {/* Create newChat window */}
             <CreateChatModal
                 isOpen={isCreateChatOpen}
                 onClose={() => setIsCreateChatOpen(false)}

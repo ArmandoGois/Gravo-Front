1df91a9aee6b8e3eb2e54389d59144595c0c8896
JD1714 <jd19122003@gmail.com>
2026-01-13 09:28:22 -0400

    feat: added background image

--- src/app/chathub/page.tsx
+++ src/app/chathub/page.tsx
@@ -2,7 +2,7 @@ import { ChatHub } from '@/presentation/components/features/chat/chat-hub';
 
 const ChatHubPage = () => (
 
-    <div className="flex min-h-screen items-center justify-center bg-linear-to-br from-zinc-50 to-zinc-100 p-4 dark:from-zinc-950 dark:to-black bg-[url('/register-bg.jpg')] bg-no-repeat">
+    <div className="flex min-h-screen items-center justify-center bg-linear-to-br from-zinc-50 to-zinc-100 p-4 dark:from-zinc-950 dark:to-black bg-[url('/chat-bg.jpg')] bg-no-repeat">
         <ChatHub />
     </div>
 );
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,7 @@
 import { RegistrationForm } from '@/presentation/components/features/auth/register-form';
 
 const RegisterPage = () => (
-    <div className="flex min-h-screen items-center justify-center bg-linear-to-br from-zinc-50 to-zinc-100 p-4 dark:from-zinc-950 dark:to-black bg-[url('/register-bg.jpg')]">
+    <div className="flex min-h-screen items-center justify-center bg-linear-to-br from-zinc-50 to-zinc-100 p-4 dark:from-zinc-950 dark:to-black bg-[url('/chat-bg.jpg')]">
         <RegistrationForm />
     </div>
 );
